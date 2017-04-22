9a481923d5fb2148da36f6b08a66d5d8c4e30d2c
vaishnavi-iyer <iyervaishnavi26@gmail.com>
2017-04-22 17:13:39 +1200

    Updated store file

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,22 +1,18 @@
-import {
-  createStore,
-  // applyMiddleware,
-  compose
-} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import rootReducer from '../reducers/example'
 
-import reducers from '../reducers'
 
-export default function configureStore (initialState) {
-  const middlewares = [
-  ]
+export default function configureStore () {
+  const logger = createLogger()
+  const middlewares = [thunk, logger]
   if (global.__DEV__) {
     if (global.reduxNativeDevTools) middlewares.push(global.reduxNativeDevTools())
   }
-  return createStore(
-    reducers,
-    initialState,
-    compose(
-      ...middlewares
-    )
+  const enhancer = compose(
+      applyMiddleware(...middlewares)
   )
+    return createStore(rootReducer, enhancer)
 }
+
