23c0cf066233ac143e289d8d2bcfdf9a7604c0b6
vaishnavi-iyer <iyervaishnavi26@gmail.com>
2017-04-22 17:26:28 +1200

    Updated the home page to include the grid and the button

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,34 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   View,
   Image,
   Text
 } from 'react-native'
+import { connect } from 'react-redux'
 import styles from './App.styles.js'
 import Topbar from '../../components/Topbar'
+import PressMeButton from '../PressMeButton'
+import Grid from '../Grid'
 
-export default () => (
-  <View style={styles.container}>
-    <Topbar />
-    <Image
-      style={{
-        width: 350,
-        height: 200,
-        paddingTop: 0,
-        marginTop: 0
-      }}
-      resizeMode={'cover'}
-      source={{ uri: 'https://unsplash.it/600/400/?random' }}
-    />
-    <Text
-      style={{
-        color: 'black',
-        fontSize: 24,
-        fontWeight: 'normal',
-        fontFamily: 'Helvetica Neue',
-        textAlign: 'center'
-      }}>
-      Welcome to this React Native Redux boiler.
-    </Text>
-  </View>
-)
+class App extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Topbar />
+      {(this.props.appState == 'SUCCESS') ? 
+        <Grid />
+       :
+      <View>
+        <PressMeButton />
+        <Image
+          style={styles.image}
+          resizeMode={'cover'}
+          source={{ uri: 'https://unsplash.it/600/400/?random' }}
+        /> 
+        <Text
+          style={styles.text}>
+          Welcome to this React Native Redux boiler.
+        </Text>
+      </View>
+      }
+      </View>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    appState: state.appState
+  }
+}
+
+
+export default connect(mapStateToProps)(App)
--- src/containers/App/App.styles.js
+++ src/containers/App/App.styles.js
@@ -7,6 +7,20 @@ export default {
     alignItems: 'center',
     backgroundColor: 'white',
     width: Dimensions.width,
-    height: Dimensions.height
-  }
+    height: Dimensions.height,
+  },
+  image: {
+    width: 350,
+    height: 200,
+    paddingTop: 0,
+    margin: 0,
+    alignSelf: 'center',
+  },
+  text: {
+  	color: 'black',
+    fontSize: 24,
+    fontWeight: 'normal',
+    fontFamily: 'Helvetica Neue',
+    textAlign: 'center',
+  },
 }
